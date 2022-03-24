b97f4d720af5308b3cb713fc7b7dc2e73329c652
roeliars <roman@fev.com.mx>
2022-03-23 21:45:24 -0600

    Se agregaron varias opciones de color para la serpiente y su comida

--- Snake.py
+++ Snake.py
@@ -1,22 +1,38 @@
-from turtle import *
+"""Snake, classic arcade game.
+Exercises
+1. How do you make the snake faster or slower?  
+2. How can you make the snake go around the edges? 
+3. How would you move the food?
+4. Change the snake to respond to mouse clicks.
+"""
+
 from random import randrange
+import random
+from turtle import *
+
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+#RMEV - 23/03/2022 - We add a list of colors and random choices
+color=['black', 'blue', 'orange', 'purple']
+c1=random.choice(color)
+c2=random.choice(color)
+
 def change(x, y):
-    "Change snake direction."
+    """Change snake direction."""
     aim.x = x
     aim.y = y
 
+
 def inside(head):
-    "Return True if head inside boundaries."
+    """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
-    "Move snake forward one segment."
+    """Move snake forward one segment."""
     head = snake[-1].copy()
     head.move(aim)
 
@@ -36,12 +52,24 @@ def move():
 
     clear()
 
+    #RMEV - 23/03/2022 - We define, that in each run, the color of the snake and the food change
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, c1)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, c2)
     update()
     ontimer(move, 100)
+
+#RAVR - 23/03/2022 - The food is moving one tile in a preselected time where it is created
+def movefood():
+
+    if inside(food):
+        food.x += (randrange(-1, 2) * 10)
+        food.y += (randrange(-1, 2) * 10)
+        
+    ontimer(movefood, 500)
+
+    
 
 setup(420, 420, 370, 0)
 hideturtle()
@@ -52,4 +80,5 @@ onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+movefood()
 done()
